9f71ef3abb5b024f6ed2d1bd58a801a1b46dbc33
imai kazusa <kazusa1304@icloud.com>
2025-12-07 15:17:25 +0900

    あ

---
+++ UI/PreviewMessageView.swift
@@ -0,0 +1,18 @@
+//
+//  PreviewMessageView.swift
+//  aikotoba
+//
+//  Created by 今井一颯 on 2025/12/07.
+//
+
+import SwiftUI
+
+struct PreviewMessageView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PreviewMessageView()
+}
