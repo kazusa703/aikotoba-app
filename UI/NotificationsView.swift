7d47f01ac1407155ebdd05f12bd33e47a8303c3a
imai kazusa <kazusa1304@icloud.com>
2025-12-10 23:36:19 +0900

    あ

---
+++ UI/NotificationsView.swift
@@ -0,0 +1,18 @@
+//
+//  NotificationsView.swift
+//  aikotoba
+//
+//  Created by 今井一颯 on 2025/12/10.
+//
+
+import SwiftUI
+
+struct NotificationsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NotificationsView()
+}
---
+++ file/AppNotification.swift
@@ -0,0 +1,8 @@
+//
+//  AppNotification.swift
+//  aikotoba
+//
+//  Created by 今井一颯 on 2025/12/10.
+//
+
+import Foundation
