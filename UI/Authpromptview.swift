fb2b5c842a26a9c2bc0fdcd44f6eca20ab0c372d
imai kazusa <kazusa1304@icloud.com>
2025-12-30 03:55:00 +0900

    j

---
+++ UI/Authpromptview.swift
@@ -0,0 +1,18 @@
+//
+//  Authpromptview.swift
+//  aikotoba
+//
+//  Created by 今井一颯 on 2025/12/30.
+//
+
+import SwiftUI
+
+struct Authpromptview: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Authpromptview()
+}
---
+++ UI/Googlelogoview.swift
@@ -0,0 +1,18 @@
+//
+//  Googlelogoview.swift
+//  aikotoba
+//
+//  Created by 今井一颯 on 2025/12/30.
+//
+
+import SwiftUI
+
+struct Googlelogoview: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Googlelogoview()
+}
